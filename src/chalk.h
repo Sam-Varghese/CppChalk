a9bbf2290b5325b9349e70d8473c49d06f8b8c82
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-04-08 10:03:15 +0530

    Added support to more colors

--- src/chalk.h
+++ src/chalk.h
@@ -2,16 +2,63 @@
 #include <windows.h>
 #include <string>
 
-class Chalk{
-    public:
-    void blue(std::string Text){
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
+class Chalk
+{
+private:
+    void SetTextColor(int colorCode, std::string Text)
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorCode);
         std::cout << Text;
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
     }
-    void green(std::string Text){
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
-        std::cout << Text;
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
+public:
+    void blue(std::string Text)
+    {
+        SetTextColor(1, Text);
+    }
+    void green(std::string Text)
+    {
+        SetTextColor(2, Text);
+    }
+    void cyan(std::string Text)
+    {
+        SetTextColor(3, Text);
+    }
+    void red(std::string Text)
+    {
+        SetTextColor(4, Text);
+    }
+    void purple(std::string Text)
+    {
+        SetTextColor(5, Text);
+    }
+    void yellow(std::string Text)
+    {
+        SetTextColor(5, Text);
+    }
+    void white(std::string Text)
+    {
+        SetTextColor(7, Text);
+    }
+    void brightBlue(std::string Text)
+    {
+        SetTextColor(9, Text);
+    }
+    void brightGreen(std::string Text)
+    {
+        SetTextColor(10, Text);
+    }
+    void brightCyan(std::string Text)
+    {
+        SetTextColor(11, Text);
+    }
+    void brightRed(std::string Text)
+    {
+        SetTextColor(12, Text);
+    }
+    void magenta(std::string Text)
+    {
+        SetTextColor(13, Text);
     }
 };
