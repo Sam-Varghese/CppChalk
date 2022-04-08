36cda356dc3d3f75b735bb2e238f54bc8dd1886f
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-04-08 12:28:02 +0530

    Adding examples

---
+++ examples/example1/example1.cpp
@@ -0,0 +1,12 @@
+#include "../src/chalk.h"
+
+int main()
+{
+    Chalk Chalk;
+    Chalk.red("Hello world\n");
+    Chalk.blue("Hello world\n");
+    Chalk.green("Hello world\n");
+    Chalk.magenta("Hello world\n");
+    Chalk.grey("Hello world\n");
+    Chalk.yellow("Hello world\n");
+}
